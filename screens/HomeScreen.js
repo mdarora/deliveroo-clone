52f2d506fcaf807a3b0c3f549e87162bf05df7ce
mdarora <mdarora0001@gmail.com>
2023-12-26 00:20:41 -0500

    added row component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
-import { View, Text, SafeAreaView, ScrollView, StyleSheet, Platform } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import HomeHeader from '../components/HomeHeader';
 import Categories from '../components/Categories';
-import {ArrowRightIcon} from 'react-native-heroicons/outline'
+import Row from '../components/Row';
 
 export default function HomeScreen() {
     const navigation = useNavigation();
@@ -21,13 +21,9 @@ export default function HomeScreen() {
             <HomeHeader />
             <ScrollView className="bg-gray-100" contentContainerStyle={{ paddingBottom: 100 }}>
                 <Categories />
-                <View className='px-2 my-2 flex-row items-center justify-between'>
-                    <View>
-                        <Text className="text-xl font-bold">Featured</Text>
-                        <Text className='text-gray-400'>Paid Placements from our partners</Text>
-                    </View>
-                    <ArrowRightIcon size={20} color='#00ccbb'/>
-                </View>
+                <Row id='1' heading='Featured' desc='Paid Placements from our partners'/>
+                <Row id='12' heading='Tasty Discounts' desc="Everyone's been enjoying these jucy discounts!"/>
+                <Row id='123' heading='Offers near you' desc='Why not support your local restaurant tonight!'/>
             </ScrollView>
         </SafeAreaView>
     )
